d7ff47a0a9df2b7d7a0d9bb0cdd48625e551638a
Polina <polinakoctyu4enko@gmail.com>
2024-12-20 13:16:42 +0200

    Hw-1

---
+++ src/generics/1.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+type Data= { userId: 1; id: 1; title: "delectus aut autem"; completed: false };
+
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
+fetchData<Data>("url");
